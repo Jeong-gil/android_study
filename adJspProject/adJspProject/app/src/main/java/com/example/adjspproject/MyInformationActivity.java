2e372e12f642311027e408173b3b659eaf6fddbb
YangJeongGil <bom20065@naver.com>
2020-11-30 01:05:46 +0900

    회원정보 수정 완료후 창 꺼짐 기능 추가, 필요없는 UI일부 제거

--- adJspProject/adJspProject/app/src/main/java/com/example/adjspproject/MyInformationActivity.java
+++ adJspProject/adJspProject/app/src/main/java/com/example/adjspproject/MyInformationActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import Network.UpdateMyInfo;
 
@@ -32,13 +33,15 @@ public class MyInformationActivity extends AppCompatActivity {
             public void onClick(View v) {
                 EditText myinfoPw = (EditText) findViewById(R.id.myinfoPw);
                 EditText myinfoName = (EditText) findViewById(R.id.myinfoName);
-                EditText myinfoGender = (EditText) findViewById(R.id.myinfoGender);
                 EditText myinfoEmail = (EditText) findViewById(R.id.myinfoEmail);
 
                 System.out.println(userID);
                 System.out.println(myinfoPw.getText().toString().trim());
 
                 new UpdateMyInfo().execute(userID, myinfoPw.getText().toString().trim(), myinfoName.getText().toString().trim(), myinfoEmail.getText().toString().trim());
+
+                Toast.makeText(getApplicationContext(), "회원정보 수정 완료", Toast.LENGTH_SHORT).show();
+                finish();
             }
         });
 
